434f3ddd8333cf5037643c6952319a89268e9530
Niraj Baniya <nbaniya314@gmail.com>
2024-11-11 08:41:29 +0545

    Lab work in c

---
+++ 2-sum-of-two-num.c
@@ -0,0 +1,11 @@
+// Write a program to add two numbers (5&7) and display its sum.
+
+#include<stdio.h>
+int main(){
+    int a = 5;
+    int b = 7;
+
+    int sum = a + b;
+    printf("The sum is: %d", sum);
+    return 0;
+}
